1c70a931262c374b8331cc5b9d86a2bddafcd1dd
ritviksc <contact4ritvik@gmail.com>
2026-07-10 18:48:05 +1200

    Fixed comment typo

--- kernel/start.c
+++ kernel/start.c
@@ -59,7 +59,7 @@ timerinit()
   w_mcounteren(r_mcounteren() | 2);
 
   // ask for the very first timer interrupt.
-  // timebase freqency of RV2 is 24MHz
+  // timebase frequency of RV2 is 24MHz
   // Target: ~~10 interrupts per second
   // Ticks: 24000000 / 10 = 2400000
   w_stimecmp(r_time() + 2400000);
