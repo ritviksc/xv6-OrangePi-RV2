7f52ca245eaab952f0b9eacda0da69e665fcb6ca
ritviksc <contact4ritvik@gmail.com>
2026-07-14 21:51:42 +1200

    Updated most required macros with appropriate hardware values including adding timer related macros.

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -1,32 +1,36 @@
-// Physical memory layout
+// Layout NOT FINAL - will need to replace qemu virtual disk
+// Using physical memory layout for the OrangePi RV2
+// This ensures that xv6 can run properly on the OrangePi RV2
 
-// qemu -machine virt is set up like this,
-// based on qemu's hw/riscv/virt.c:
-//
-// 00001000 -- boot ROM, provided by qemu
-// 02000000 -- CLINT
-// 0C000000 -- PLIC
-// 10000000 -- uart0
+// Using the .dts file for the OrangePi RV2
+
+// E4000000 -- CLINT
+// E0000000 -- PLIC
+// d4017000 -- UART0
 // 10001000 -- virtio disk
 // 80000000 -- qemu's boot ROM loads the kernel here,
 //             then jumps here.
 // unused RAM after 80000000.
 
 // the kernel uses physical memory thus:
-// 80000000 -- entry.S, then kernel text and data
+// 0x11000000 -- entry.S, then kernel text and data
 // end -- start of kernel page allocation area
 // PHYSTOP -- end RAM used by the kernel
 
-// qemu puts UART registers here in physical memory.
-#define UART0     0x10000000L
-#define UART0_IRQ 10
+#define DEVICE_NAME "OrangePi RV2" 
+
+#define TIMEBASE_FREQ 24000000L  // 24MHz
+#define TIMER_HZ 10 // timer interrupts per second (~100ms scheduling granularity like QEMU xv6)
+#define TIMEBASE_FREQ/TIMER_HZ // clock ticks between one timer interrupt and the next
+
+#define UART0     0xd4017000L
+#define UART0_IRQ 42
 
 // virtio mmio interface
-#define VIRTIO0     0x10001000
-#define VIRTIO0_IRQ 1
+// #define VIRTIO0     0x10001000
+// #define VIRTIO0_IRQ 1
 
-// qemu puts platform-level interrupt controller (PLIC) here.
-#define PLIC                 0x0c000000L
+#define PLIC                 0xE0000000L
 #define PLIC_PRIORITY        (PLIC + 0x0)
 #define PLIC_PENDING         (PLIC + 0x1000)
 #define PLIC_SENABLE(hart)   (PLIC + 0x2080 + (hart) * 0x100)
@@ -35,8 +39,10 @@
 
 // the kernel expects there to be RAM
 // for use by the kernel and user pages
-// from physical address 0x80000000 to PHYSTOP.
-#define KERNBASE 0x80000000L
+// from physical address 0x11000000 to PHYSTOP.
+#define KERNBASE 0x11000000L
+
+// For inital port let system only have 128MB of free RAM
 #define PHYSTOP  (KERNBASE + 128 * 1024 * 1024)
 
 // map the trampoline page to the highest address,
