6c40497b42973cd5f1ae061237b0d257dabc3744
ritviksc <contact4ritvik@gmail.com>
2026-07-14 21:49:37 +1200

    Print what harts don't wake up

--- kernel/main.c
+++ kernel/main.c
@@ -6,7 +6,7 @@
 
 volatile static int started = 0;
 
-// start() jumps here in supervisor mode on all CPUs.
+// start() jumps here on all CPUs eventually.
 void
 main()
 {
@@ -14,9 +14,20 @@ main()
     consoleinit();
     printkinit();
     printk("\n");
-    printk("DEVICE:OrangePi RV2\n");
+    printk("DEVICE:%s\n",DEVICE_NAME);
     printk("xv6 kernel is booting\n");
     printk("\n");
+
+    // Check if any harts are not starting
+    // and report them and continue as normal.
+    // If required to be implemented, let user decide to continue using device
+    // or quit.
+    for (int i = 1; i < NCPU; i++) {
+      if (hart_start_failed[i]) {
+        printk("hart %d failed to start\n", i);
+      }
+    }
+
     kinit();            // physical page allocator
     kvminit();          // create kernel page table
     kvminithart();      // turn on paging
@@ -28,7 +39,7 @@ main()
     binit();            // buffer cache
     iinit();            // inode table
     fileinit();         // file table
-    virtio_disk_init(); // emulated hard disk
+    virtio_disk_init(); // emulated hard disk - will need to replace this
     userinit();         // first user process
     __atomic_thread_fence(__ATOMIC_SEQ_CST);
     started = 1;
@@ -36,6 +47,7 @@ main()
     while (started == 0)
       ;
     __atomic_thread_fence(__ATOMIC_SEQ_CST);
+
     printk("hart %d starting\n", cpuid());
     kvminithart();  // turn on paging
     trapinithart(); // install kernel trap vector
