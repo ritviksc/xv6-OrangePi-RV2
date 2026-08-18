639846e8fff4d0827eb5586b0f8f162848b9cf37
Ritvik Sharma <shari838@student.otago.ac.nz>
2026-08-18 15:37:29 +1200

    Added SBI typedefs

--- kernel/include/xv6/types.h
+++ kernel/include/xv6/types.h
@@ -1,4 +1,5 @@
-#include <stdbool.h> // freestanding header
+#ifndef TYPES_H
+#define TYPES_H
 
 typedef unsigned int uint;
 typedef unsigned short ushort;
@@ -10,3 +11,11 @@ typedef unsigned int uint32;
 typedef unsigned long uint64;
 
 typedef uint64 pde_t;
+
+// SBI typedefs
+typedef int sbi_eid_t; // signed 32-bit value
+typedef int sbi_fid_t; // signed 32-bit value
+typedef long sbi_ret_t; // XLEN width
+typedef unsigned long sbi_arg_t; // XLEN width
+
+#endif
